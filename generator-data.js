a804e045ed142b2c6229e2ec6f38e99e7fc51ec9
cryssycat <crysthigpen@gmail.com>
2026-06-23 20:35:24 -0400

    Update generator-data.js

--- generator-data.js
+++ generator-data.js
@@ -46093,7 +46093,8 @@ storyHooks: [
   "Their promise rings are keys to another world.",
   "A tiny spirit village builds itself inside their garden.",
   "They discover that they have already saved the world once."
-]
+] };
+  
 const VIBE_DATA = {
 
 themes: [
