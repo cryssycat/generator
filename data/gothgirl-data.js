cc7f3b2a4ae87e4aa8b6f2cbc234e027f6699503
cryssycat <crysthigpen@gmail.com>
2026-06-30 10:04:07 -0400

    Create gothgirl-data.js

---
+++ data/gothgirl-data.js
@@ -0,0 +1,126 @@
+const GOTH_DATA = {
+
+    species: [
+        "Human",
+        "Ghost Fox",
+        "Bat",
+        "Cat",
+        "Wolf",
+        "Raven",
+        "Bunny",
+        "Demon",
+        "Vampire",
+        "Moth"
+    ],
+
+    aesthetics: [
+        "Victorian Goth",
+        "Pastel Goth",
+        "Romantic Goth",
+        "Mall Goth",
+        "Nu Goth",
+        "Cyber Goth",
+        "Gothic Lolita",
+        "Dark Academia",
+        "Witchcore",
+        "Vampire Chic"
+    ],
+
+    hair: [
+        "Long Black",
+        "White Bob",
+        "Split Black & White",
+        "Dark Purple",
+        "Silver",
+        "Midnight Blue",
+        "Burgundy",
+        "Deep Emerald"
+    ],
+
+    eyes: [
+        "Crimson",
+        "Silver",
+        "Ice Blue",
+        "Purple",
+        "Golden",
+        "Black",
+        "Emerald",
+        "Rose Pink"
+    ],
+
+    outfits: [
+        "Velvet Dress",
+        "Lace Blouse",
+        "Corset",
+        "Oversized Sweater",
+        "Victorian Coat",
+        "Long Skirt",
+        "Layered Dress",
+        "Dark Suit"
+    ],
+
+    accessories: [
+        "Bat Necklace",
+        "Moon Earrings",
+        "Cross Pendant",
+        "Lace Gloves",
+        "Silver Rings",
+        "Black Umbrella",
+        "Pocket Watch",
+        "Crystal Choker"
+    ],
+
+    personalities: [
+        "Quiet",
+        "Gentle",
+        "Elegant",
+        "Sarcastic",
+        "Dreamy",
+        "Protective",
+        "Mysterious",
+        "Bookworm"
+    ],
+
+    hobbies: [
+        "Reading",
+        "Tea",
+        "Cemetery Walks",
+        "Painting",
+        "Music",
+        "Photography",
+        "Writing Poetry",
+        "Collecting Antiques"
+    ],
+
+    favoriteWeather: [
+        "Rain",
+        "Fog",
+        "Snow",
+        "Thunderstorms",
+        "Cloudy Days",
+        "Autumn Evenings"
+    ],
+
+    occupations: [
+        "Librarian",
+        "Florist",
+        "Tailor",
+        "Artist",
+        "Tea Witch",
+        "Musician",
+        "Apothecary",
+        "Fashion Designer"
+    ],
+
+    storyHooks: [
+        "Can hear whispers from old books.",
+        "Collects cursed jewelry.",
+        "Lives inside an abandoned cathedral.",
+        "Protects a haunted library.",
+        "Always followed by ravens.",
+        "Can weave moonlight into clothing.",
+        "Makes enchanted tea.",
+        "Writes letters to forgotten ghosts."
+    ]
+
+};
