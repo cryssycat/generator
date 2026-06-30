ca5c8203158b57e1a313fbc5676afab9a783e723
cryssycat <crysthigpen@gmail.com>
2026-06-30 10:37:44 -0400

    Create outfit-data.js

---
+++ data/outfit-data.js
@@ -0,0 +1,136 @@
+const OUTFIT_DATA = {
+
+    headwear: [
+        "Nothing",
+        "Beanie",
+        "Wide-Brim Hat",
+        "Witch Hat",
+        "Flower Crown",
+        "Halo",
+        "Hood",
+        "Tiara",
+        "Hair Ribbon",
+        "Cat Ear Headband",
+        "Beret",
+        "Bonnet",
+        "Bandana",
+        "Circlet",
+        "Antlers"
+    ],
+
+    hairstyles: [
+        "Long Straight",
+        "Twin Tails",
+        "Messy Bob",
+        "Braided",
+        "Curly",
+        "Wolf Cut",
+        "Ponytail",
+        "Pixie Cut",
+        "Side Braid",
+        "Flowing Waves",
+        "Space Buns",
+        "Half-Up Style"
+    ],
+
+    tops: [
+        "Oversized Sweater",
+        "Blouse",
+        "Corset",
+        "Hoodie",
+        "Kimono",
+        "Cardigan",
+        "Leather Jacket",
+        "Cloak",
+        "Dress Shirt",
+        "Fantasy Tunic",
+        "Off-Shoulder Top",
+        "Victorian Blouse"
+    ],
+
+    bottoms: [
+        "Long Skirt",
+        "Pleated Skirt",
+        "Shorts",
+        "Jeans",
+        "Wide Pants",
+        "Leggings",
+        "Layered Skirt",
+        "Flowing Robe",
+        "Cargo Pants",
+        "Bell Bottoms"
+    ],
+
+    dresses: [
+        "None",
+        "Sundress",
+        "Ball Gown",
+        "Lolita Dress",
+        "Victorian Dress",
+        "Tea Dress",
+        "Fairy Dress",
+        "Princess Gown",
+        "Gothic Dress",
+        "Wrap Dress"
+    ],
+
+    footwear: [
+        "Combat Boots",
+        "Sneakers",
+        "Mary Janes",
+        "Heeled Boots",
+        "Sandals",
+        "Barefoot",
+        "Platform Boots",
+        "Loafers",
+        "Ankle Boots",
+        "Knee High Boots"
+    ],
+
+    accessories: [
+        "Scarf",
+        "Choker",
+        "Moon Necklace",
+        "Crystal Pendant",
+        "Satchel",
+        "Backpack",
+        "Fingerless Gloves",
+        "Long Gloves",
+        "Bracelets",
+        "Silver Rings",
+        "Lantern",
+        "Parasol",
+        "Book",
+        "Pocket Watch",
+        "Nothing"
+    ],
+
+    colorPalettes: [
+        "Black & Crimson",
+        "Lavender & Silver",
+        "Mint & Cream",
+        "Brown & Gold",
+        "Blue & White",
+        "Pink & White",
+        "Forest Green",
+        "Autumn Orange",
+        "Teal & Gold",
+        "Rose & Burgundy"
+    ],
+
+    aesthetics: [
+        "Cozy",
+        "Fantasy",
+        "Dark Academia",
+        "Pastel Goth",
+        "Cottagecore",
+        "Royal",
+        "Steampunk",
+        "Modern Casual",
+        "Cyberpunk",
+        "Victorian",
+        "Witchcore",
+        "Fairycore"
+    ]
+
+};
