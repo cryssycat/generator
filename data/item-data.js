0fa7dfb0c869d73728af8e3a4e8ed1172b5e6ac5
cryssycat <crysthigpen@gmail.com>
2026-06-30 10:46:52 -0400

    Create item-data.js

---
+++ data/item-data.js
@@ -0,0 +1,179 @@
+const ITEM_DATA = {
+
+    categories: [
+
+        "Book",
+        "Lantern",
+        "Sword",
+        "Potion",
+        "Ring",
+        "Necklace",
+        "Bracelet",
+        "Backpack",
+        "Journal",
+        "Coffee Mug",
+        "Teapot",
+        "Pocket Watch",
+        "Camera",
+        "Paintbrush",
+        "Umbrella",
+        "Music Box",
+        "Mirror",
+        "Key",
+        "Map",
+        "Bottle",
+        "Crystal",
+        "Gemstone",
+        "Stuffed Animal",
+        "Scarf",
+        "Hat",
+        "Candle",
+        "Notebook",
+        "Spellbook",
+        "Compass",
+        "Flower Pot"
+
+    ],
+
+    materials: [
+
+        "Wood",
+        "Oak",
+        "Steel",
+        "Iron",
+        "Silver",
+        "Gold",
+        "Glass",
+        "Crystal",
+        "Leather",
+        "Velvet",
+        "Silk",
+        "Clay",
+        "Porcelain",
+        "Bone",
+        "Dragon Scale",
+        "Moonstone",
+        "Starlight Crystal",
+        "Obsidian",
+        "Marble"
+
+    ],
+
+    colors: [
+
+        "Black",
+        "White",
+        "Cream",
+        "Chocolate Brown",
+        "Crimson",
+        "Emerald",
+        "Sky Blue",
+        "Lavender",
+        "Mint",
+        "Rose Pink",
+        "Silver",
+        "Gold"
+
+    ],
+
+    conditions: [
+
+        "Brand New",
+        "Well Loved",
+        "Ancient",
+        "Dusty",
+        "Polished",
+        "Broken",
+        "Cracked",
+        "Restored",
+        "Enchanted",
+        "Weathered"
+
+    ],
+
+    qualities: [
+
+        "Ordinary",
+        "Rare",
+        "Handmade",
+        "Mastercrafted",
+        "Royal",
+        "Enchanted",
+        "Cursed",
+        "Blessed",
+        "Legendary"
+
+    ],
+
+    enchantments: [
+
+        "None",
+        "Glows softly.",
+        "Never gets dirty.",
+        "Always warm.",
+        "Always cold.",
+        "Whispers secrets.",
+        "Stores memories.",
+        "Repairs itself.",
+        "Changes color.",
+        "Floats beside its owner.",
+        "Produces tiny stars.",
+        "Creates flowers."
+
+    ],
+
+    owners: [
+
+        "A Traveling Merchant",
+        "A Young Witch",
+        "An Ancient Dragon",
+        "A Librarian",
+        "A Lost Prince",
+        "A Ghost",
+        "A Baker",
+        "A Forest Spirit",
+        "A Retired Knight",
+        "A Child"
+
+    ],
+
+    locations: [
+
+        "Hidden Library",
+        "Old Attic",
+        "Forest Shrine",
+        "Abandoned Castle",
+        "Flower Field",
+        "Moon Temple",
+        "Tea Shop",
+        "Mountain Peak",
+        "Crystal Cave",
+        "Marketplace"
+
+    ],
+
+    storyHooks: [
+
+        "Contains a forgotten memory.",
+
+        "Appears whenever someone needs it.",
+
+        "Was crafted over 500 years ago.",
+
+        "Has passed through countless owners.",
+
+        "Leads to hidden treasure.",
+
+        "Cannot be destroyed.",
+
+        "Disappears every full moon.",
+
+        "Once belonged to a legendary hero.",
+
+        "Protects its owner from nightmares.",
+
+        "Can only be found by accident."
+
+    ]
+
+};
