3ebddbc4fb05a5955e761028b122b1fc4b231d6e
cryssycat <crysthigpen@gmail.com>
2026-06-30 10:59:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ const generators = {
    outfit: outfitGenerator,
    weapon: weaponGenerator,
    item: itemGenerator,
-   magic: magicGenerator
+   magic: magicGenerator,
+   polycule: polyculeGenerator
 };
 
 /* ---------------------------------------------------------
