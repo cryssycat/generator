82fd3763a751a5de1ad5d8fec8727c0c6773b28e
cryssycat <crysthigpen@gmail.com>
2026-06-30 10:07:51 -0400

    Create magicalgirl-data.js

---
+++ data/magicalgirl-data.js
@@ -0,0 +1,138 @@
+const MAGICAL_GIRL_DATA = {
+
+    names: [
+        "Aurora",
+        "Luna",
+        "Celeste",
+        "Nova",
+        "Iris",
+        "Ember",
+        "Sakura",
+        "Aster",
+        "Rosalie",
+        "Seraphina"
+    ],
+
+    animals: [
+        "Fox",
+        "Bunny",
+        "Cat",
+        "Wolf",
+        "Bear",
+        "Bat",
+        "Butterfly",
+        "Deer",
+        "Dragon",
+        "Moth"
+    ],
+
+    themes: [
+        "Stars",
+        "Moon",
+        "Sun",
+        "Flowers",
+        "Dreams",
+        "Music",
+        "Tea",
+        "Candy",
+        "Ocean",
+        "Snow",
+        "Rainbows",
+        "Crystals"
+    ],
+
+    colors: [
+        "Pink",
+        "Blue",
+        "Purple",
+        "White",
+        "Gold",
+        "Silver",
+        "Mint",
+        "Lavender",
+        "Red",
+        "Black"
+    ],
+
+    outfits: [
+        "Frilly Dress",
+        "Military Uniform",
+        "Princess Gown",
+        "Magical School Uniform",
+        "Elegant Kimono",
+        "Celestial Robes",
+        "Fairy Dress",
+        "Royal Armor"
+    ],
+
+    weapons: [
+        "Magic Wand",
+        "Scythe",
+        "Bow",
+        "Sword",
+        "Ribbon",
+        "Staff",
+        "Umbrella",
+        "Spellbook",
+        "Lantern",
+        "Crystal Orb"
+    ],
+
+    powers: [
+        "Healing",
+        "Time Magic",
+        "Moonlight",
+        "Stars",
+        "Ice",
+        "Fire",
+        "Nature",
+        "Dream Magic",
+        "Light",
+        "Music"
+    ],
+
+    companions: [
+        "Tiny Fox",
+        "Floating Cat",
+        "Moon Rabbit",
+        "Baby Dragon",
+        "Living Plushie",
+        "Owl",
+        "Fairy",
+        "Spirit Wolf"
+    ],
+
+    personalities: [
+        "Kind",
+        "Energetic",
+        "Gentle",
+        "Determined",
+        "Shy",
+        "Cheerful",
+        "Elegant",
+        "Optimistic"
+    ],
+
+    finishers: [
+        "Starlight Finale",
+        "Moonbeam Burst",
+        "Rainbow Bloom",
+        "Celestial Nova",
+        "Crystal Symphony",
+        "Aurora Strike",
+        "Dream Blossom",
+        "Heavenly Waltz"
+    ],
+
+    storyHooks: [
+        "Chosen by an ancient guardian spirit.",
+        "Protects dreams while everyone sleeps.",
+        "Collects shattered stars.",
+        "Searching for the lost Queen of Magic.",
+        "Can hear the wishes of children.",
+        "Must balance school and saving the world.",
+        "Her transformation grows stronger with hope.",
+        "Every spell creates a new constellation."
+    ]
+
+};
