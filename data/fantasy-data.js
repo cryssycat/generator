fbb1ac50f54f168a8b0545de4932081be184ae79
cryssycat <crysthigpen@gmail.com>
2026-06-30 09:32:10 -0400

    Create fantasy-data.js

---
+++ data/fantasy-data.js
@@ -0,0 +1,93 @@
+const FANTASY_DATA = {
+
+    races: [
+        "Human",
+        "Elf",
+        "Dark Elf",
+        "Dwarf",
+        "Halfling",
+        "Orc",
+        "Goblin",
+        "Dragonborn",
+        "Tiefling",
+        "Fairy",
+        "Merfolk",
+        "Kitsune",
+        "Satyr",
+        "Angel",
+        "Demon"
+    ],
+
+    classes: [
+        "Knight",
+        "Mage",
+        "Paladin",
+        "Ranger",
+        "Rogue",
+        "Cleric",
+        "Druid",
+        "Necromancer",
+        "Bard",
+        "Monk",
+        "Summoner",
+        "Alchemist"
+    ],
+
+    weapons: [
+        "Long Sword",
+        "Great Axe",
+        "Bow",
+        "Crossbow",
+        "Twin Daggers",
+        "Magic Staff",
+        "Spear",
+        "Scythe",
+        "Hammer",
+        "Spellbook"
+    ],
+
+    armor: [
+        "Heavy Plate",
+        "Leather Armor",
+        "Robes",
+        "Chainmail",
+        "Scale Armor",
+        "Enchanted Cloak"
+    ],
+
+    elements: [
+        "Fire",
+        "Ice",
+        "Lightning",
+        "Nature",
+        "Shadow",
+        "Light",
+        "Arcane",
+        "Water",
+        "Wind",
+        "Earth"
+    ],
+
+    abilities: [
+        "Time Magic",
+        "Healing",
+        "Teleportation",
+        "Shape Shifting",
+        "Summoning",
+        "Blood Magic",
+        "Illusions",
+        "Barrier Magic",
+        "Beast Taming",
+        "Runes"
+    ],
+
+    goals: [
+        "Slay a Dragon",
+        "Protect the Kingdom",
+        "Find Ancient Relics",
+        "Master Forbidden Magic",
+        "Become Guild Master",
+        "Break an Ancient Curse"
+    ]
+
+};
