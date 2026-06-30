de2f90885d35194347c3daa991d27e7d068fd9a4
cryssycat <crysthigpen@gmail.com>
2026-06-30 10:16:37 -0400

    Create mlp-data.js

---
+++ mlp-data.js
@@ -0,0 +1,151 @@
+const MLP_DATA = {
+
+    tribes: [
+        "Earth Pony",
+        "Pegasus",
+        "Unicorn",
+        "Alicorn",
+        "Crystal Pony",
+        "Bat Pony",
+        "Kirin",
+        "Seapony",
+        "Hippogriff",
+        "Zebra"
+    ],
+
+    bodyColors: [
+        "Snow White",
+        "Sky Blue",
+        "Lavender",
+        "Mint Green",
+        "Rose Pink",
+        "Golden Yellow",
+        "Midnight Blue",
+        "Emerald",
+        "Coral",
+        "Soft Gray",
+        "Chocolate Brown",
+        "Cream"
+    ],
+
+    maneColors: [
+        "Rainbow",
+        "Silver",
+        "Pink & White",
+        "Purple & Blue",
+        "Black & Red",
+        "Mint & Aqua",
+        "Lavender & White",
+        "Gold & Orange",
+        "Blue & Pink",
+        "Crimson"
+    ],
+
+    maneStyles: [
+        "Long Flowing",
+        "Curly",
+        "Short & Messy",
+        "Twin Braids",
+        "High Ponytail",
+        "Elegant Waves",
+        "Spiky",
+        "Straight",
+        "Side Braid",
+        "Fluffy"
+    ],
+
+    eyeColors: [
+        "Blue",
+        "Green",
+        "Amber",
+        "Purple",
+        "Pink",
+        "Gold",
+        "Silver",
+        "Red",
+        "Teal",
+        "Brown"
+    ],
+
+    cutieMarks: [
+        "Tea Cup",
+        "Paintbrush",
+        "Crystal Star",
+        "Moon",
+        "Book",
+        "Music Note",
+        "Flower",
+        "Potion Bottle",
+        "Lantern",
+        "Butterfly",
+        "Snowflake",
+        "Compass",
+        "Acorn",
+        "Cupcake",
+        "Feather"
+    ],
+
+    talents: [
+        "Magic",
+        "Cooking",
+        "Music",
+        "Storytelling",
+        "Healing",
+        "Gardening",
+        "Flying",
+        "Crafting",
+        "Exploration",
+        "Fashion"
+    ],
+
+    personalities: [
+        "Kind",
+        "Energetic",
+        "Shy",
+        "Optimistic",
+        "Curious",
+        "Protective",
+        "Gentle",
+        "Brave",
+        "Dreamy",
+        "Cheerful"
+    ],
+
+    homes: [
+        "Ponyville",
+        "Canterlot",
+        "Cloudsdale",
+        "Crystal Empire",
+        "Everfree Forest",
+        "Manehattan",
+        "Appleloosa",
+        "Seaside Village",
+        "Mountain Monastery",
+        "Hidden Cottage"
+    ],
+
+    occupations: [
+        "Baker",
+        "Librarian",
+        "Florist",
+        "Teacher",
+        "Potion Maker",
+        "Artist",
+        "Mail Pony",
+        "Weather Team",
+        "Musician",
+        "Dressmaker"
+    ],
+
+    dreams: [
+        "Open the greatest bakery in Equestria.",
+        "Discover forgotten magic.",
+        "Travel the world.",
+        "Help everypony smile.",
+        "Protect ancient forests.",
+        "Become a royal scholar.",
+        "Create magical art.",
+        "Build a peaceful village."
+    ]
+
+};
