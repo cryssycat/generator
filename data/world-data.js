ebbb5f9fa5074d3f940a3a68a023037aca23bb3e
cryssycat <crysthigpen@gmail.com>
2026-06-30 11:01:20 -0400

    Create world-data.js

---
+++ data/world-data.js
@@ -0,0 +1,171 @@
+const WORLD_DATA = {
+
+    worldNames: [
+
+        "Asteria",
+        "Moonveil",
+        "Everbloom",
+        "Lunaris",
+        "Eldoria",
+        "Starhaven",
+        "Mistfall",
+        "Auravale",
+        "Silvergrove",
+        "Dreamreach",
+        "Crystalmere",
+        "Solstice"
+
+    ],
+
+    worldTypes: [
+
+        "Fantasy Realm",
+        "Floating Islands",
+        "Ancient Kingdom",
+        "Modern Fantasy",
+        "Post-Apocalyptic Magic",
+        "Sky Kingdom",
+        "Underground Civilization",
+        "Ocean World",
+        "Endless Forest",
+        "Celestial Realm"
+
+    ],
+
+    climates: [
+
+        "Temperate",
+        "Always Spring",
+        "Frozen",
+        "Tropical",
+        "Desert",
+        "Stormy",
+        "Perpetual Autumn",
+        "Perpetual Night",
+        "Crystal Winter",
+        "Floating Clouds"
+
+    ],
+
+    landscapes: [
+
+        "Crystal Mountains",
+        "Ancient Forest",
+        "Flower Meadows",
+        "Volcanic Wasteland",
+        "Moonlit Swamp",
+        "Sky Islands",
+        "Golden Plains",
+        "Frozen Tundra",
+        "Mystic Jungle",
+        "Glowing Caves"
+
+    ],
+
+    governments: [
+
+        "Monarchy",
+        "Council of Mages",
+        "Village Elders",
+        "Merchant Guild",
+        "Democracy",
+        "Dragon Empire",
+        "Spirit Guardians",
+        "Independent City States"
+
+    ],
+
+    races: [
+
+        "Humans",
+        "Elves",
+        "Dragons",
+        "Fox Spirits",
+        "Beastfolk",
+        "Fairies",
+        "Dwarves",
+        "Giants",
+        "Merfolk",
+        "Living Constructs"
+
+    ],
+
+    magicLevels: [
+
+        "Rare",
+        "Common",
+        "Everyday",
+        "Forbidden",
+        "Lost",
+        "Unstable",
+        "Ancient",
+        "Divine"
+
+    ],
+
+    magicSources: [
+
+        "Stars",
+        "Moon",
+        "Nature",
+        "Dreams",
+        "Ancient Gods",
+        "Crystals",
+        "Dragons",
+        "Music",
+        "Runes",
+        "The Ocean"
+
+    ],
+
+    threats: [
+
+        "Ancient Dragon",
+        "Corrupted Forest",
+        "Endless Winter",
+        "Dark Empire",
+        "Lost Magic",
+        "Living Shadows",
+        "Monster Invasion",
+        "Civil War",
+        "The Void",
+        "Time Fractures"
+
+    ],
+
+    landmarks: [
+
+        "The World Tree",
+        "Sky Palace",
+        "Crystal Cathedral",
+        "Endless Library",
+        "Moon Temple",
+        "Sunken City",
+        "Tower of Stars",
+        "The Great Forge",
+        "Frozen Castle",
+        "Garden of Spirits"
+
+    ],
+
+    legends: [
+
+        "A sleeping god rests beneath the world.",
+
+        "Every shooting star grants a wish.",
+
+        "The first dragon still watches over the kingdom.",
+
+        "Magic disappears for one day every century.",
+
+        "An invisible city appears once each year.",
+
+        "Every forest has a guardian spirit.",
+
+        "A forgotten hero will return.",
+
+        "The moon hides an ancient civilization."
+
+    ]
+
+};
