4faa0266c3c0506360eada58d58d023658ed9eec
cryssycat <crysthigpen@gmail.com>
2026-06-30 09:46:59 -0400

    Create relationship-data.js

---
+++ data/relationship-data.js
@@ -0,0 +1,86 @@
+const RELATIONSHIP_DATA = {
+
+    characterTypes: [
+
+        "Ghost Fox",
+        "Tea Bunny",
+        "Forest Wolf",
+        "Moon Deer",
+        "Crystal Dragon",
+        "Flower Cat",
+        "Bat",
+        "Sheep",
+        "Goat",
+        "Bear"
+
+    ],
+
+    personalities: [
+
+        "Kind",
+        "Shy",
+        "Outgoing",
+        "Protective",
+        "Gentle Giant",
+        "Playful",
+        "Stoic",
+        "Dreamy"
+
+    ],
+
+    relationships: [
+
+        "Best Friends",
+        "Childhood Friends",
+        "Enemies",
+        "Friendly Rivals",
+        "Business Partners",
+        "Siblings",
+        "Parent & Child",
+        "Mentor & Student",
+        "Travel Companions",
+        "Roommates"
+
+    ],
+
+    conflicts: [
+
+        "One keeps a dangerous secret.",
+        "They disagree on magic.",
+        "A misunderstanding separated them.",
+        "They compete for the same goal.",
+        "Neither trusts outsiders.",
+        "One broke an important promise."
+
+    ],
+
+    sharedInterests: [
+
+        "Tea",
+        "Collecting Books",
+        "Stargazing",
+        "Gardening",
+        "Potion Brewing",
+        "Cooking",
+        "Music",
+        "Exploring Ruins"
+
+    ],
+
+    storyHooks: [
+
+        "Searching for a legendary relic together.",
+
+        "Protecting a magical village.",
+
+        "Trying to restore an ancient forest.",
+
+        "Running a tiny café together.",
+
+        "Following clues left by an old friend.",
+
+        "Trying to solve a magical mystery."
+
+    ]
+
+};
