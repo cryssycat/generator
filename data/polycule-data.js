d1a0005553d60f1c008a628a6061df80bd351741
cryssycat <crysthigpen@gmail.com>
2026-06-30 10:57:16 -0400

    Create polycule-data.js

---
+++ data/polycule-data.js
@@ -0,0 +1,139 @@
+const POLYCULE_DATA = {
+
+    species: [
+
+        "Ghost Fox",
+        "Tea Bunny",
+        "Bat",
+        "Wolf",
+        "Cat",
+        "Dragon",
+        "Sheep",
+        "Goat",
+        "Bear",
+        "Otter",
+        "Moth",
+        "Raven"
+
+    ],
+
+    personalities: [
+
+        "Kind",
+        "Shy",
+        "Outgoing",
+        "Protective",
+        "Dreamy",
+        "Playful",
+        "Gentle Giant",
+        "Elegant",
+        "Bookworm",
+        "Chaotic"
+
+    ],
+
+    occupations: [
+
+        "Artist",
+        "Tea Witch",
+        "Florist",
+        "Librarian",
+        "Baker",
+        "Fashion Designer",
+        "Apothecary",
+        "Musician",
+        "Traveler",
+        "Potion Maker"
+
+    ],
+
+    hobbies: [
+
+        "Reading",
+        "Painting",
+        "Gardening",
+        "Tea Blending",
+        "Cooking",
+        "Photography",
+        "Stargazing",
+        "Collecting Crystals"
+
+    ],
+
+    dynamics: [
+
+        "Childhood Friends",
+
+        "Found Family",
+
+        "Adventure Partners",
+
+        "Business Owners",
+
+        "Housemates",
+
+        "Traveling Together",
+
+        "Magical Coven",
+
+        "Festival Performers",
+
+        "Cafe Owners",
+
+        "Protectors of an Ancient Forest"
+
+    ],
+
+    strengths: [
+
+        "Excellent Communication",
+
+        "Balances Each Other Perfectly",
+
+        "Always Solves Problems Together",
+
+        "Shares Every Responsibility",
+
+        "Never Gives Up On Each Other",
+
+        "Supports Individual Dreams"
+
+    ],
+
+    challenges: [
+
+        "Busy Schedules",
+
+        "Different Love Languages",
+
+        "Long Distance",
+
+        "Protecting Their Secret",
+
+        "Conflicting Goals",
+
+        "Learning To Trust"
+
+    ],
+
+    storyHooks: [
+
+        "Running a magical tea shop together.",
+
+        "Searching for ancient relics.",
+
+        "Protecting a hidden village.",
+
+        "Building a cozy home.",
+
+        "Traveling the world collecting memories.",
+
+        "Trying to break an ancient curse.",
+
+        "Guardians of a celestial library.",
+
+        "Opening the greatest bakery in the kingdom."
+
+    ]
+
+};
