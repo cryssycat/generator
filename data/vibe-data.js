ee957301318d11c0ab1f21a50f33cbab1951009e
cryssycat <crysthigpen@gmail.com>
2026-07-15 08:35:01 -0400

    Create vibe-data.js

---
+++ data/vibe-data.js
@@ -0,0 +1,18 @@
+const VIBE_DATA = {
+
+    aesthetics: [
+
+        "Cozy",
+        "Soft Goth",
+        "Royalcore",
+        "Dreamcore",
+        "Fairycore",
+        "Dark Academia",
+        "Light Academia",
+        "Cottagecore",
+        "Whimsigoth",
+        "Art Nouveau"
+
+    ]
+
+};
