7c5014e83ec70e02dd90a6e74a31b3304dc43239
cryssycat <crysthigpen@gmail.com>
2026-07-15 09:47:03 -0400

    Create inventory-data.js

---
+++ data/inventory-data.js
@@ -0,0 +1,439 @@
+/* ==========================================
+   Inventory Generator Data
+========================================== */
+
+const INVENTORY_DATA = {
+
+    bags: [
+
+        "Leather Satchel",
+        "Canvas Backpack",
+        "Messenger Bag",
+        "Mini Backpack",
+        "Drawstring Bag",
+        "Crossbody Bag",
+        "Travel Pack",
+        "Potion Satchel",
+        "Book Bag",
+        "Wicker Basket",
+        "Picnic Basket",
+        "Adventurer's Pack",
+        "Magic Pouch",
+        "Treasure Chest Backpack",
+        "Fox-Shaped Backpack"
+
+    ],
+
+    keyItems: [
+
+        "Skeleton Key",
+        "Ancient Coin",
+        "Magic Compass",
+        "Pocket Watch",
+        "Family Crest",
+        "Spellbook",
+        "Old Photograph",
+        "Crystal Orb",
+        "Moon Pendant",
+        "Star Map",
+        "Guild Badge",
+        "Music Box",
+        "Silver Locket",
+        "Tiny Bell",
+        "Journal"
+
+    ],
+
+    luckyCharms: [
+
+        "Four Leaf Clover",
+        "Rabbit Foot Charm",
+        "Lucky Coin",
+        "Pressed Flower",
+        "Crystal Pendant",
+        "Wooden Fox Charm",
+        "Tiny Dreamcatcher",
+        "Lucky Feather",
+        "Seashell",
+        "Glass Marble",
+        "Tiny Acorn",
+        "Moon Charm",
+        "Star Charm",
+        "Ribbon Bracelet",
+        "Friendship Bracelet"
+
+    ],
+
+    notebooks: [
+
+        "Leather Journal",
+        "Spellbook",
+        "Travel Diary",
+        "Sketchbook",
+        "Recipe Book",
+        "Nature Log",
+        "Pocket Notebook",
+        "Dream Journal",
+        "Adventure Log",
+        "Blank Journal",
+        "Pressed Flower Journal",
+        "Research Notes",
+        "Poetry Book",
+        "Field Guide",
+        "Map Book"
+
+    ],
+
+    writingTools: [
+
+        "Fountain Pen",
+        "Quill",
+        "Mechanical Pencil",
+        "Charcoal Pencil",
+        "Ink Brush",
+        "Colored Pencil",
+        "Magic Quill",
+        "Glass Pen",
+        "Feather Pen",
+        "Wooden Pencil",
+        "Gold Pen",
+        "Silver Pen"
+
+    ],
+
+    snacks: [
+
+        "Chocolate Bar",
+        "Honey Cookies",
+        "Trail Mix",
+        "Fresh Apple",
+        "Dried Fruit",
+        "Rice Balls",
+        "Moon Cakes",
+        "Jerky",
+        "Granola Bar",
+        "Strawberry Pocky",
+        "Caramel Candy",
+        "Muffin",
+        "Cupcake",
+        "Bag of Pretzels",
+        "Cheese Crackers"
+
+    ],
+
+    drinks: [
+
+        "Water Bottle",
+        "Chamomile Tea",
+        "Coffee",
+        "Hot Chocolate",
+        "Berry Juice",
+        "Apple Cider",
+        "Lemonade",
+        "Milk Tea",
+        "Potion Flask",
+        "Herbal Tea",
+        "Sparkling Water",
+        "Fruit Smoothie",
+        "Orange Juice",
+        "Energy Drink",
+        "Iced Tea"
+
+    ],
+
+    emergencySupplies: [
+
+        "Bandages",
+        "First Aid Kit",
+        "Healing Herbs",
+        "Antidote",
+        "Pain Reliever",
+        "Emergency Blanket",
+        "Flashlight",
+        "Matches",
+        "Extra Batteries",
+        "Sewing Kit",
+        "Hand Sanitizer",
+        "Magic Bandage",
+        "Emergency Rations",
+        "Compass",
+        "Whistle"
+
+    ],
+
+    currency: [
+
+        "Coin Pouch",
+        "Leather Wallet",
+        "Gold Coins",
+        "Silver Coins",
+        "Copper Coins",
+        "Credit Card",
+        "Guild Tokens",
+        "Gemstone Chips",
+        "Traveler's Checks",
+        "Ancient Currency"
+
+    ],
+
+    utilityItems: [
+
+        "Pocket Knife",
+        "Lantern",
+        "Rope",
+        "Compass",
+        "Multi-tool",
+        "Binoculars",
+        "Magnifying Glass",
+        "Flint & Steel",
+        "Lockpicks",
+        "Torch",
+        "Pocket Mirror",
+        "Map",
+        "Magic Crystal",
+        "Notebook Clip",
+        "Tiny Telescope"
+
+    ],
+
+    weapons: [
+
+        "Dagger",
+        "Short Sword",
+        "Long Sword",
+        "Bow",
+        "Crossbow",
+        "Magic Staff",
+        "Wand",
+        "Battle Axe",
+        "Spear",
+        "Scythe",
+        "Rapier",
+        "Throwing Knives",
+        "War Hammer",
+        "Katana",
+        "None"
+
+    ],
+
+    tools: [
+
+        "Hammer",
+        "Fishing Rod",
+        "Pickaxe",
+        "Shovel",
+        "Hatchet",
+        "Lockpick Set",
+        "Alchemy Kit",
+        "Cooking Kit",
+        "Sewing Kit",
+        "Paint Brush",
+        "Lantern Hook",
+        "Magic Toolkit",
+        "Repair Kit",
+        "Climbing Gear",
+        "Woodworking Tools"
+
+    ],
+
+    navigation: [
+
+        "Compass",
+        "Folded Map",
+        "Treasure Map",
+        "Star Chart",
+        "Magic Compass",
+        "Pocket GPS",
+        "Travel Guide",
+        "Ancient Map",
+        "Celestial Chart",
+        "Guild Map"
+
+    ],
+
+    potions: [
+
+        "Healing Potion",
+        "Mana Potion",
+        "Stamina Potion",
+        "Strength Potion",
+        "Invisibility Potion",
+        "Luck Potion",
+        "Speed Potion",
+        "Antidote",
+        "Sleep Potion",
+        "Love Potion"
+
+    ],
+
+    survival: [
+
+        "Tent",
+        "Sleeping Bag",
+        "Firestarter",
+        "Cooking Pot",
+        "Water Filter",
+        "Fishing Kit",
+        "Rope",
+        "Blanket",
+        "Portable Stove",
+        "Emergency Shelter"
+
+    ],
+
+    keepsakes: [
+
+        "Family Photograph",
+        "Wedding Ring",
+        "Childhood Toy",
+        "Pressed Flower",
+        "Old Letter",
+        "Silver Locket",
+        "Lucky Coin",
+        "Friendship Bracelet",
+        "Music Box",
+        "Tiny Plush"
+
+    ],
+
+    memories: [
+
+        "Concert Ticket",
+        "Train Ticket",
+        "Festival Pass",
+        "Old Map",
+        "Photo Strip",
+        "Dried Flower",
+        "Love Letter",
+        "Vacation Postcard",
+        "Journal Entry",
+        "Bookmark"
+
+    ],
+
+    gifts: [
+
+        "Knitted Scarf",
+        "Stuffed Bunny",
+        "Handmade Bracelet",
+        "Wooden Carving",
+        "Flower Bouquet",
+        "Chocolate Box",
+        "Magic Charm",
+        "Pocket Watch",
+        "Handwritten Letter",
+        "Tiny Painting"
+
+    ],
+
+    comfortItems: [
+
+        "Favorite Hoodie",
+        "Plush Fox",
+        "Blanket",
+        "Tea Tin",
+        "Pillow",
+        "Music Box",
+        "Stuffed Dragon",
+        "Lucky Scarf",
+        "Cozy Sweater",
+        "Soft Gloves"
+
+    ],
+
+    secretItems: [
+
+        "Hidden Letter",
+        "Ancient Key",
+        "Mysterious Crystal",
+        "Secret Map",
+        "Treasure Clue",
+        "Magic Ring",
+        "Guild Contract",
+        "Fake Passport",
+        "Encrypted Journal",
+        "Unknown Artifact"
+
+    ],
+
+    trinkets: [
+
+        "Tiny Bell",
+        "Glass Marble",
+        "Mini Lantern",
+        "Acorn",
+        "Shell",
+        "Crystal Shard",
+        "Feather",
+        "Tiny Clock",
+        "Wooden Fox",
+        "Lucky Dice"
+
+    ],
+
+    companionItems: [
+
+        "Dog Treats",
+        "Cat Toy",
+        "Bird Seed",
+        "Brush",
+        "Leash",
+        "Pet Blanket",
+        "Tiny Collar",
+        "Magic Familiar Treats",
+        "Water Bowl",
+        "Favorite Toy"
+
+    ],
+
+    valuables: [
+
+        "Diamond Ring",
+        "Gold Necklace",
+        "Emerald Brooch",
+        "Ruby Pendant",
+        "Silver Crown",
+        "Ancient Relic",
+        "Rare Coin",
+        "Crystal Orb",
+        "Family Heirloom",
+        "Golden Key"
+
+    ],
+
+    collectibles: [
+
+        "Postcards",
+        "Stamps",
+        "Coins",
+        "Gemstones",
+        "Pressed Flowers",
+        "Tea Cups",
+        "Feathers",
+        "Music Boxes",
+        "Vintage Keys",
+        "Mini Figurines"
+
+    ],
+
+    strangeObjects: [
+
+        "Glowing Mushroom",
+        "Tiny Dragon Egg",
+        "Living Marble",
+        "Bottomless Bottle",
+        "Clock That Runs Backwards",
+        "Whispering Feather",
+        "Floating Crystal",
+        "Pocket Star",
+        "Bottle of Moonlight",
+        "Unknown Key",
+        "Mini Black Hole",
+        "Talking Acorn",
+        "Cloud in a Jar",
+        "Never-Melting Snowflake",
+        "Compass That Spins Constantly"
+
+    ]
+
+};
