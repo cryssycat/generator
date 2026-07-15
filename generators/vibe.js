e9cbe07e15c8b72db7b4984ed3570839df6d1777
cryssycat <crysthigpen@gmail.com>
2026-07-15 08:35:43 -0400

    Create vibe.js

---
+++ generators/vibe.js
@@ -0,0 +1,17 @@
+const vibeGenerator = {
+
+    name: "Vibe Generator",
+
+    fields: [
+
+        {
+            id: "aesthetic",
+            label: "Overall Aesthetic",
+            icon: "✨",
+            group: "Visual",
+            options: VIBE_DATA.aesthetics
+        }
+
+    ]
+
+};
